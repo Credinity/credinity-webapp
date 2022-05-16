ccb675fabdc0cf22db3ed314993d9ecc310f9ca0
Nat <natthaset2538@gmail.com>
2022-05-17 00:28:50 +0700

    [Update] Forgot Password Page

--- pages/forgotPassword.tsx
+++ pages/forgotPassword.tsx
@@ -12,7 +12,6 @@ import {
 //#endregion
 
 //#region UI Components
-import styles from "../styles/Home.module.css";
 import {
     Grid,
     TextField,
@@ -47,7 +46,6 @@ import {
     Google,
     ArrowDropDown,
 } from "@mui/icons-material";
-import { CredinityLogoImg } from "../public/static/images";
 //#endregion
 
 //#region Types
@@ -56,6 +54,9 @@ import { CredinityLogoImg } from "../public/static/images";
 //#region service
 import axios from "axios";
 import { AnyAaaaRecord } from "dns";
+import LanguageChanger from "@/components/input/LanguageChanger";
+import PageContainer from "@/components/layout/pageContainer";
+import CredinityTextField from "@/components/input/CredinityTextField";
 //#endregion
 
 const LoginPage: NextPage = () => {
@@ -65,7 +66,7 @@ const LoginPage: NextPage = () => {
     const [showPassword, setShowPassword]: [boolean, Function] =
         useState(false);
     const [error, setError]: [string, Function] = useState("");
-    const [requestSuccess, setRequestSuccess]: [bool, Function] =
+    const [requestSuccess, setRequestSuccess]: [boolean, Function] =
         useState(false);
     const [isLoading, setIsLoading]: [boolean, Function] = useState(false);
 
@@ -96,7 +97,7 @@ const LoginPage: NextPage = () => {
         setShowPassword(!showPassword);
     };
     return (
-        <TempContainer>
+        <PageContainer>
             <Grid
                 container
                 direction="column"
@@ -105,12 +106,17 @@ const LoginPage: NextPage = () => {
                 sx={{ px: "40px" }}
             >
                 <Grid item>
-                    <Typography variant="h1" align="center">
+                    <Typography
+                        variant="h1"
+                        align="center"
+                        fontSize="1.4rem"
+                        fontWeight="medium"
+                    >
                         Reset Password
                     </Typography>
                 </Grid>
-                <Grid item sx={{ mt: 1 }}>
-                    <Typography variant="body1" align="center" color="#9A9A9A">
+                <Grid item sx={{ mt: 1, px: 1 }}>
+                    <Typography variant="body2" align="center" color="#9A9A9A">
                         Enter your registered email address with credinity and
                         we will email you a link to reset your password
                     </Typography>
@@ -128,7 +134,13 @@ const LoginPage: NextPage = () => {
                         }}
                     />
                 </Grid>
-                <Grid item container xs={12} justifyContent="center">
+                <Grid
+                    item
+                    container
+                    xs={12}
+                    sx={{ mt: 2 }}
+                    justifyContent="center"
+                >
                     {isLoading ? (
                         <CircularProgress />
                     ) : requestSuccess ? (
@@ -150,143 +162,7 @@ const LoginPage: NextPage = () => {
                 </Grid>
             </Grid>
             <LanguageChanger />
-        </TempContainer>
+        </PageContainer>
     );
 };
 export default LoginPage;
-
-const TempContainer: any = (props: any) => {
-    const theme = createTheme();
-    theme.typography.h1 = {
-        fontSize: "1.2rem",
-    };
-    theme.typography.body1 = {
-        fontSize: "0.8rem",
-    };
-    theme.typography["label"] = {
-        fontSize: "0.8rem",
-    };
-
-    return (
-        <ThemeProvider theme={theme}>
-            <div style={{ height: "100vh" }}>
-                <Head>
-                    <title>Login</title>
-                    <link rel="icon" href="/favicon.ico" />
-                </Head>
-                {props.children}
-            </div>
-        </ThemeProvider>
-    );
-};
-
-const CredinityLabel: FunctionComponent<any> = (props: any) => {
-    let baseStyle = { ...props.style, fontWeight: "bold" };
-    return <Typography variant="label" {...props} style={baseStyle} />;
-};
-
-const CredinityPillButton: FunctionComponent<any> = (props: any) => {
-    let baseStyle = {
-        ...props.style,
-        textTransform: "none",
-        width: "100%",
-        borderRadius: "50px",
-    };
-    console.log(props);
-    return (
-        <Button
-            variant="outlined"
-            color="secondary"
-            style={baseStyle}
-            {...props}
-        >
-            {props.children}
-        </Button>
-    );
-};
-
-const CredinityTextField: FunctionComponent<any> = ({
-    label,
-    labelProps,
-    textFieldProps,
-}: {
-    label: string;
-    labelProps: any;
-    textFieldProps: TextFieldProps;
-}) => {
-    return (
-        <>
-            {label ? <CredinityLabel>{label}</CredinityLabel> : null}
-            <TextField
-                sx={{ my: "10px" }}
-                variant="outlined"
-                name="email"
-                fullWidth
-                inputProps={{
-                    style: { padding: "5px 20px 5px 20px" },
-                }}
-                {...textFieldProps}
-            />
-        </>
-    );
-};
-
-const LanguageChanger: FunctionComponent<any> = (props: any) => {
-    const [lang, setLang] = useState("EN");
-    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-    const handleChangeLang = (newLang: string) => {
-        setLang(newLang);
-        handleClose();
-    };
-
-    const getFullLang = (langAbbrv: string) => {
-        switch (langAbbrv) {
-            case "TH":
-                return "Thai";
-            case "EN":
-            default:
-                return "English";
-        }
-    };
-    return (
-        <Grid
-            container
-            style={{ position: "absolute", bottom: "20px" }}
-            justifyContent="center"
-        >
-            <Grid item xs="auto">
-                <Typography display="inline" fontWeight="bold">
-                    Choose Language:
-                </Typography>
-                <Button
-                    onClick={handleClick}
-                    style={{
-                        textTransform: "none",
-                        color: "black",
-                        fontWeight: "bold",
-                    }}
-                >
-                    {getFullLang(lang)} <ArrowDropDown />
-                </Button>
-                <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
-                    <MenuItem onClick={(e) => handleChangeLang("EN")}>
-                        English
-                    </MenuItem>
-                    <MenuItem
-                        onClick={(e) => handleChangeLang("TH")}
-                        disabled={true}
-                    >
-                        Thai
-                    </MenuItem>
-                </Menu>
-            </Grid>
-        </Grid>
-    );
-};
