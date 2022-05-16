155e6fa2a00ad0db17b3c58d93b2bab8c9a48cab
Nat <natthaset2538@gmail.com>
2022-05-17 00:17:05 +0700

    [Update] use css in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,20 +5,25 @@ import { theme } from "styles/credinityTheme";
 import Head from "next/head";
 import { store } from "@/store/store";
 import { Provider } from "react-redux";
+import "@/public/css/Home.module.css";
+import "@/public/css/globals.css";
 
 function CredinityApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </Provider>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="initial-scale=1, width=device-width"
+                />
+            </Head>
+            <Provider store={store}>
+                <ThemeProvider theme={theme}>
+                    <Component {...pageProps} />
+                </ThemeProvider>
+            </Provider>
+        </>
+    );
 }
 
 export default CredinityApp;
