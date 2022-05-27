c344b30898955c4f98bc64d61f025c9736bcb003
Chairat <mr.chairat_lucky@icloud.com>
2022-05-27 15:25:42 +0700

    [Update] Index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,21 @@
-import {
-    userSelector,
-    // resetUsername,
-    signUpAsync,
-} from "@/store/slices/userSlice";
-import { useAppDispatch } from "@/store/store";
-import { Button } from "@mui/material";
-import Link from "next/link";
-import React from "react";
+import AppBarHeader from "@/components/layout/AppBarHeader";
+import PageContainer from "@/components/layout/pageContainer";
+import { userSelector } from "@/store/slices/userSlice";
+import { Grid } from "@mui/material";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
 type Props = {};
 
 export default function Index({}: Props) {
-    const user = useSelector(userSelector);
-    const dispatch = useAppDispatch();
-    return (
-        <>
-            {/* <div>Index {user.username}</div>
-            <button onClick={() => dispatch(resetUsername({ data: "555" }))}>
-                Reset
-            </button> 
-
-            <button
-                onClick={() =>
-                    dispatch(
-                        signUpAsync({ username: "admin", password: "1234" })
-                    )
-                }
-            >
-                signUpAsync
-            </button> */}
-
-            <Link href="/auth/signIn">
-                <Button>Sign In</Button>
-            </Link>
-            <Link href="/auth/signOut">
-                <Button>Sign Out</Button>
-            </Link>
-            <Link href="/user/profile">
-                <Button>Profile</Button>
-            </Link>
-        </>
-    );
+  const user = useSelector(userSelector);
+  return (
+    <PageContainer
+      pageName="Index"
+      loading={user.isRequestSuccess}
+      loadingMessage="Redirecting..."
+    >
+      <AppBarHeader />
+    </PageContainer>
+  );
 }
