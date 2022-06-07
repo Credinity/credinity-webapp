88e8ea09da3d0688a128bfb5c5a657ad8a8665cb
Chairat <mr.chairat_lucky@icloud.com>
2022-06-07 15:03:06 +0700

    [Update] Verify email page

--- pages/auth/verifyemail.tsx
+++ pages/auth/verifyemail.tsx
@@ -1,7 +1,63 @@
+import PageContainer from "@/components/layout/PageContainer";
+import { userSelector } from "@/store/slices/userSlice";
+import { useAppDispatch } from "@/store/store";
+import { Box, Button, Grid, Link, Paper, Typography } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
 
-type Props = {};
-
-export default function VerifyEmailPage({}: Props) {
-  return <div>verify email</div>;
+export default function VerifyEmailPage() {
+  const dispatch = useAppDispatch();
+  const user = useSelector(userSelector);
+  return (
+    <PageContainer
+      pageName="Sign Up"
+      loading={user.isRequestSuccess}
+      loadingMessage="Redirecting..."
+    >
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Paper elevation={3} sx={{ padding: 3 }}>
+          <Grid item xs={12} sx={{ mx: 4 }}>
+            <Typography variant="h4" align="center" fontWeight="bold">
+              Verify Your Email Address
+            </Typography>
+            <Typography variant="h5" align="center" sx={{ mt: 2 }}>
+              To continue using Credinity {<br />} please verify your email
+              address.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2, mx: 4 }}>
+            <Button
+              type="button"
+              variant="contained"
+              color="primary"
+              fullWidth
+              disabled={user.isProcessing}
+            >
+              <Typography>VERIFY EMAIL</Typography>
+            </Button>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sx={{ mt: 2 }}
+            justifyContent="center"
+            alignItems="center"
+            display="flex"
+          >
+            <Typography display="inline" variant="h5" sx={{ mr: 1 }}>
+              <Link href="" color="primary">
+                Trouble verifying?
+              </Link>
+            </Typography>
+          </Grid>
+        </Paper>
+      </Grid>
+    </PageContainer>
+  );
 }
