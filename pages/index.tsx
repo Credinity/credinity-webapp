56e4f43ff042cb4260761eab7e70d1e4698b70d6
Chairat <mr.chairat_lucky@icloud.com>
2022-06-24 23:17:45 +0700

    [Update] Main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,10 @@ import PageContainer from "@/components/layouts/PageContainer";
 import { setRequestSuccess, userSelector } from "@/store/slices/userSlice";
 import { useAppDispatch } from "@/store/store";
 import {
+  Box,
   CircularProgress,
+  Divider,
   Grid,
-  Paper,
   Stack,
   Typography,
 } from "@mui/material";
@@ -14,12 +15,19 @@ import { useRouter } from "next/router";
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import Image from "next/image";
-import MainPageImg from "../public/img/contents/mainpageImg.png";
+import ImgDetailCard from "@/components/layouts/ImgDetailCard";
+import { Black } from "@/public/constants/color.constant";
 import PrimaryButton from "@/components/inputs/PrimaryButton";
+import MainPageImg from "../public/img/contents/mainpageImg.png";
+import Auction from "../public/img/features/Auction.png";
+import AuctionHover from "../public/img/features/AuctionHover.png";
+import Auto from "../public/img/features/Auto.png";
+import AutoHover from "../public/img/features/AutoHover.png";
+import Anyone from "../public/img/features/Anyone.png";
+import AnyoneHover from "../public/img/features/AnyoneHover.png";
+
 export default function Index() {
   const user = useSelector(userSelector);
-  const [isReadMore, setIsReadMore]: [boolean, Function] = useState(false);
-  const [isSignIn, setSignIn]: [boolean, Function] = useState(false);
   const [isSignUp, setSignUp]: [boolean, Function] = useState(false);
   const dispatch = useAppDispatch();
   const router = useRouter();
@@ -28,47 +36,7 @@ export default function Index() {
     router.push(path);
     dispatch(setRequestSuccess(false));
   };
-  const moreCards = () => {
-    return (
-      <>
-        {/* todo: change to list map cards from services */}
-        <Grid item container sx={{ paddingY: 1 }}>
-          {/* <Image
-        src=""
-        alt=""
-        width={1900}
-        height={1600}
-      /> */}
 
-          <Paper
-            elevation={3}
-            sx={{
-              minWidth: "100%",
-              minHeight: "55vh",
-              backgroundColor: "aqua",
-            }}
-          />
-        </Grid>
-        <Grid item container>
-          {/* <Image
-         src=""
-         alt=""
-         width={1900}
-         height={1600}
-       /> */}
-
-          <Paper
-            elevation={3}
-            sx={{
-              minWidth: "100%",
-              minHeight: "55vh",
-              backgroundColor: "pink",
-            }}
-          />
-        </Grid>
-      </>
-    );
-  };
   const menuArray = [
     { name: "LOG IN", path: "/auth/signIn" },
     { name: "REGISTER", path: "/auth/signUp" },
@@ -91,25 +59,23 @@ export default function Index() {
         spacing={1}
         sx={{ px: "20px" }}
       >
-        <Typography variant="h3" textAlign="center">
-          Credinity, a P2P lending and investment service platform
-        </Typography>
-        <Image
-          priority
-          src={MainPageImg}
-          alt="Main Photo"
-          layout="responsive"
-          width={800}
-          height={1000}
-        />
-        <Typography variant="caption" textAlign="start">
-          เราเป็นตัวกลางในการทำให้
-          ผู้ขอสินเชื่อและผู้ให้สินเชื่อมาพบกันโดยที่มีสินทรัพย์ค้ำประกัน
-          โดยเราจะเป็นผู้จัดการเอกสาร
-          รวมถึงรองรับความเสี่ยงของสินเชื่อให้กับผู้ให้สินเชื่อ
-          สนใจร่วมเป็นส่วนหนึ่งของเรา
-        </Typography>
-        <Grid item container sx={{ paddingY: 1 }}>
+        <Box sx={{ mb: 2 }}>
+          <Typography variant="h1" fontWeight="bold" textAlign="center">
+            Credinity, a P2P lending and investment service platform
+          </Typography>
+          <Box sx={{ my: 2 }}>
+            <Image priority src={MainPageImg} alt="Main Photo" />
+          </Box>
+
+          <Typography variant="body1" textAlign="start">
+            เราเป็นตัวกลางในการทำให้
+            ผู้ขอสินเชื่อและผู้ให้สินเชื่อมาพบกันโดยที่มีสินทรัพย์ค้ำประกัน
+            โดยเราจะเป็นผู้จัดการเอกสาร
+            รวมถึงรองรับความเสี่ยงของสินเชื่อให้กับผู้ให้สินเชื่อ
+            สนใจร่วมเป็นส่วนหนึ่งของเรา
+          </Typography>
+        </Box>
+        <Grid item container>
           {isSignUp ? (
             <Stack alignItems="center">
               <CircularProgress />
@@ -127,8 +93,38 @@ export default function Index() {
             </PrimaryButton>
           )}
         </Grid>
-        <CredinityFullFooter />
+        <Box minHeight="15px" />
+        <Typography variant="h2">การบริการของเรา</Typography>
+        <Divider sx={{ backgroundColor: Black, my: 2 }} />
+        <Box minHeight="15px" />
+        <ImgDetailCard
+          imgAlt="auction"
+          imgNormal={Auction}
+          imgHover={AuctionHover}
+          title="Auction"
+          detail="บริการประมูลทรัพย์สิน สมาชิกมีสิทธิในการประมูลอสังหาริมทรัพย์โดยเสนอจำนวนเงิน ดอกเบี้ย และค่าธรรมเนียมปากถุง"
+        />
+        <Box minHeight="15px" />
+        <ImgDetailCard
+          imgAlt="auto exchange"
+          imgNormal={Auto}
+          imgHover={AutoHover}
+          title="Auto"
+          detail="บริการอัติโนมัติ สมาชิกสามารถโอนเงินเข้าลงทุนที่บริษัท โดยทางบริษัท Credinity จะทำการขอสินเชื่อกับสมาชิกที่ต้องการปล่อนสินเชื่อ และจะทำการนำเงินไปปล่อยสินเชื่อต่อให้สมาชิก โดยสมาชิกจะได้รับดอกเบี้ย อัติโนมัติ โดยทาง Credinity จะเป็นผู้รับความเสี่ยงให้สมาชิก ในกรณีทรัพย์สินถูกยึด"
+        />
+        <Box minHeight="15px" />
+        <ImgDetailCard
+          imgAlt="Asset Allocation and Diversification"
+          imgNormal={Anyone}
+          imgHover={AnyoneHover}
+          title="Anyone"
+          detail="บริการกระจายความเสี่ยงการปล่อยสินเชื่อ ทางผู้ปล่อยสินเชื่อ กรณีที่มีเงินไม่เพียงพอในการปล่อยสินเชื่อขนาดใหญ่ แต่ต้องการเลือกทรัพย์สินในการค้ำประกันเอง สามารถเลือกสินทรัพย์ที่บริษัทประกาศได้ว่าสินทรัพย์นี้ สามารถร่วมกระจายความเสี่ยงได้ ซึ่งสัญญาจะถูกทำในนามบริษัท เมื่อจำนวนเงินครบตามจำนวน และหากทรัพย์สินถูกยึดจะได้รับเงินคืนเมื่อขายได้เท่านั้น"
+        />
+        <Box minHeight="15px" />
+        <Typography variant="h2">บทความ</Typography>
+        <Divider sx={{ backgroundColor: Black, my: 2 }} />
       </Stack>
+      <CredinityFullFooter />
     </PageContainer>
   );
 }
