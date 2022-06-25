373b62c546c26bda0025162d44c517dbcbc9fd09
Chairat <mr.chairat_lucky@icloud.com>
2022-06-25 15:43:49 +0700

    [Add] article page

--- components/layouts/ArticleCard.tsx
+++ components/layouts/ArticleCard.tsx
@@ -1,13 +1,14 @@
-import { Box, Paper, Stack, Typography } from "@mui/material";
+import { Box, Link, Paper, Stack, Typography } from "@mui/material";
 import Image, { StaticImageData } from "next/image";
 import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import { CardActionArea } from "@mui/material";
-import ReadMoreText from "@/components/displays/ReadMoreText";
+import { useRouter } from "next/router";
+import { Secondary } from "@/public/constants/color.constant";
 
 type Props = {
+  contentId: string;
   imgContent: string;
   altImg: string;
   title: string;
@@ -15,23 +16,31 @@ type Props = {
 };
 
 export default function ArticleCard(props: Props) {
+  const router = useRouter();
   return (
     <Card variant="outlined" sx={{ padding: 2, borderRadius: 4 }}>
-      <CardActionArea>
-        <CardMedia component="img" src={props.imgContent} alt="green iguana" />
-        <CardContent>
-          <Typography
-            gutterBottom
-            variant="h3"
-            component="div"
-            fontWeight="bold"
-            sx={{ wordWrap: "break-word" }}
-          >
-            {props.title}
-          </Typography>
-          <ReadMoreText variant="body2">{props.detail}</ReadMoreText>
-        </CardContent>
-      </CardActionArea>
+      <CardMedia component="img" src={props.imgContent} alt={props.altImg} />
+      <CardContent>
+        <Typography
+          gutterBottom
+          variant="h3"
+          component="div"
+          fontWeight="bold"
+          sx={{ wordWrap: "break-word" }}
+        >
+          {props.title}
+        </Typography>
+        {/* <ReadMoreText variant="body2">{props.detail}</ReadMoreText> */}
+
+        <Typography sx={{ display: "inline", width: "100%" }}>
+          {props.detail.slice(0, 120)}
+          <Box component="span">
+            <Link color={Secondary} href="/content/article">
+              {"... อ่านเพิ่ม >>"}
+            </Link>
+          </Box>
+        </Typography>
+      </CardContent>
     </Card>
   );
 }
