574bff24a7983edabd5035eac127505395f309f1
Chairat <mr.chairat_lucky@icloud.com>
2022-07-04 18:12:43 +0700

    [Add] Back button to Appbar

--- components/layouts/AppBarHeader.tsx
+++ components/layouts/AppBarHeader.tsx
@@ -5,18 +5,27 @@ import Toolbar from "@mui/material/Toolbar";
 import DrawerMenu from "@/components/displays/DrawerMenu";
 import { White } from "@/public/constants/color.constant";
 import Image from "next/image";
-import { MenuArrItem } from "@/models/page.model";
+import BackButton from "@/components/inputs/BackButton";
 
-export default function AppBarHeader() {
+type Props = {
+  isShowBackButton?: boolean;
+};
+
+export default function AppBarHeader(props: Props) {
   return (
     <AppBar position="static" elevation={0} sx={{ backgroundColor: White }}>
       <Toolbar>
-        <Image
-          alt="logo"
-          src="/img/credinity-tr-logo.png"
-          width={40}
-          height={40}
-        />
+        {props.isShowBackButton ? (
+          <BackButton />
+        ) : (
+          <Image
+            alt="logo"
+            src="/img/credinity-tr-logo.png"
+            width={40}
+            height={40}
+          />
+        )}
+
         <Box sx={{ flexGrow: 1 }} />
         <DrawerMenu />
       </Toolbar>
