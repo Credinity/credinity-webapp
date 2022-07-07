43f2606581704662960763a878617902bc9f1780
Chairat <mr.chairat_lucky@icloud.com>
2022-07-07 15:45:29 +0700

    [Update] move CreadinityLoader to components

--- components/layouts/PageContainer.tsx
+++ components/layouts/PageContainer.tsx
@@ -8,6 +8,7 @@ import {
 } from "@mui/material";
 import Head from "next/head";
 import { FunctionComponent } from "react";
+import CreadinityLoader from "@/components/displays/CreadinityLoader";
 
 const PageContainer: FunctionComponent<any> = (props: any) => {
   return (
@@ -39,52 +40,5 @@ const PageContainer: FunctionComponent<any> = (props: any) => {
     </Box>
   );
 };
-const CreadinityLoader: FunctionComponent<any> = ({
-  loadingMessage,
-}: {
-  loadingMessage: string;
-}) => {
-  return (
-    <Grid
-      container
-      justifyContent="center"
-      alignContent="center"
-      alignItems="center"
-      style={{
-        zIndex: 999,
-        position: "fixed",
-        left: 0,
-        top: 0,
-        right: 0,
-        bottom: 0,
-        display: "flex",
-        backgroundColor: "rgba(50, 50, 50, 0.50)",
-      }}
-    >
-      <Card>
-        <CardContent sx={{ p: 3 }}>
-          <Grid
-            container
-            justifyContent="center"
-            alignContent="center"
-            alignItems="center"
-          >
-            <Grid item xs="auto" alignSelf="center">
-              <CircularProgress />
-            </Grid>
-            <Grid item xs={12} alignSelf="center">
-              <Typography
-                variant="h6"
-                align="center"
-                style={{ marginTop: "10px" }}
-              >
-                {loadingMessage ?? "Loading..."}
-              </Typography>
-            </Grid>
-          </Grid>
-        </CardContent>
-      </Card>
-    </Grid>
-  );
-};
+
 export default PageContainer;
